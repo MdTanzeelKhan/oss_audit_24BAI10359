e5db99beae3a247af7abf09aa5c3022e9e64a50f
MdTanzeelKhan <mdtanzeelkhancae902d@gmail.com>
2026-03-31 19:32:31 +0530

    Update author and student name in script1.sh

--- script1.sh
+++ script1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Script 1: System Identity Report
-# Author: Krishna (24BCE10678)
+# Author: Tanzeel (24BAI10359)
 # Course: Open Source Software
 # Software Choice: Git
 
 # --- Variables ---
 # Specific details for audit 
-STUDENT_NAME="Krishna"
+STUDENT_NAME="Tanzeel"
 SOFTWARE_CHOICE="Git"
 
 # --- System info ---
